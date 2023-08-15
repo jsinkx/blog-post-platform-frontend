be011bc1ecb52c01e0549bf6c734a20fa8d2cc71
Artemiy <andrikv@gmail.com>
2023-08-16 02:45:54 +0300

    Created types for asset files

---
+++ src/@types/assets.d.ts
@@ -0,0 +1,29 @@
+declare module '*.woff2' {
+	const content: any
+	export default content
+}
+
+declare module '*.ttf' {
+	const content: any
+	export default content
+}
+
+declare module '*.svg' {
+	const content: any
+	export default content
+}
+
+declare module '*.png' {
+	const content: any
+	export default content
+}
+
+declare module '*.webp' {
+	const content: any
+	export default content
+}
+
+declare module '*.scss' {
+	const content: any
+	export default content
+}
